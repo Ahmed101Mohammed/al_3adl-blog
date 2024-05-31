5a162c889f1bd0688521e4ef44a55fd1ae14edb1
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-05-31 11:32:20 +0300

    Adding body parser and logger middlewares.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const logger = require("./middlewares/logger");
 
 // app
 const app = express();
 
 // middlewares:
 app.use(cors());
+app.use(express.json());
+app.use(logger);
 
 // routes
 
