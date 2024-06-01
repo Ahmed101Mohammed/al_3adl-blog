36b76a9d84a5a4d98ffb3a3313d8b01493a3941d
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-06-01 22:28:34 +0300

    Adding new request with method get '/api/users/refreshToken'.

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,13 @@
 const { join } = require("node:path");
 const router = require("express").Router();
-const { register, sign, logout } = require(join(__dirname, "..", "controllers", "auth.controller"));
-const { getUsers, postUser, getUser, updateUser, deleteUser } = require(join(__dirname, "..", "controllers", "user.controller"))
+const { register, sign, logout, refreshTokenHandler } = require(join(__dirname, "..", "controllers", "auth.controller"));
+const { getUsers, getUser, updateUser, deleteUser } = require(join(__dirname, "..", "controllers", "user.controller"))
 
 router
     .post("/register", register)
     .post("/sign", sign)
-    .post("/logout", logout);
+    .post("/logout", logout)
+    .get("/refreshToken", refreshTokenHandler);
 
 router.route("/")
     .get(getUsers)
