4d28fdde4d16a7021ffd8c0b54655cf1c263bffd
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-06-14 12:03:33 +0300

    Adding controler determine appearance of signIn, and user-avater links.

---
+++ public/scripts/library/header.js
@@ -0,0 +1,52 @@
+const signInButtonAppearance = (status)=>
+{
+    let signInButton = document.querySelector("header nav a.sign-link");
+    if(status)
+    {
+        signInButton.style.display = "inline";
+    }
+    else
+    {
+        signInButton.style.removeProperty("display");
+    }
+}
+
+const userAvatarApearance = (status)=>
+{
+    let userAvatar = document.querySelector("header nav a.user-home-page-link");
+    if(status)
+    {
+        userAvatar.style.display = "inline";
+    }
+    else
+    {
+        userAvatar.style.removeProperty("display");
+    }
+}
+
+const userAvatarOrSignInWillAppear = async()=>
+{
+    try
+    {
+        const accessToken = await getNewAccessToken();
+        if(accessToken !== false)
+        {
+            signInButtonAppearance(false);
+            userAvatarApearance(true);
+        }
+        else
+        {
+            signInButtonAppearance(true);
+            userAvatarApearance(false);
+        }
+        
+    }
+    catch(e)
+    {
+        console.error(e);
+        signInButtonAppearance(true);
+        userAvatarApearance(false);
+    }
+}
+
+userAvatarOrSignInWillAppear();
